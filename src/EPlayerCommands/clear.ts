946b4bdabfe5042a20bb4305f8bb6bd37c9e80be
GhexterCortes <ghextershumies@gmail.com>
2022-06-26 01:02:25 +0800

    update

--- src/EPlayerCommands/clear.ts
+++ src/EPlayerCommands/clear.ts
@@ -1,75 +1,48 @@
-import { Queue } from 'discord-player';
 import { GuildMember } from 'discord.js';
-import { isNumber } from 'fallout-utility';
 import { InteractionCommandBuilder, MessageCommandBuilder, recipleCommandBuilders } from 'reciple';
 import { EPlayer } from '../e-player';
 
 export default (Player: EPlayer): recipleCommandBuilders[] => {
-    const skip = (queue: Queue, track?: number): string|boolean => {
-        if (track) track =  track - 1 < 0 ? 0 : track - 1;
-        if (track && track > queue.tracks.length) return 'trackNotFound';
-
-        const isSkipped = typeof track !== 'undefined' ? queue.skipTo(track) : queue.skip();
-        return isSkipped === undefined ? true : isSkipped;
-    }
-
     return [
         new InteractionCommandBuilder()
-            .setName('skip')
-            .addNumberOption(skipTo => skipTo
-                .setName('skip-to')
-                .setDescription('Skip to track number')
-                .setMinValue(1)
-            )
+            .setName('clear')
             .setExecute(async command => {
                 const interaction = command.interaction;
                 const member = interaction.member as GuildMember;
                 const guild = interaction.guild;
-                const track = interaction.options.getNumber('skip-to') ?? undefined;
                 if (!guild || !member) return interaction.reply({ embeds: [Player.getMessageEmbed('notAMember')] });
 
                 const queue = Player.player.getQueue(guild);
                 if (!queue || queue.destroyed) return interaction.reply({ embeds: [Player.getMessageEmbed('noQueue')] });
                 if (member.voice.channelId !== guild.me?.voice.channelId) return interaction.reply({ embeds: [Player.getMessageEmbed('InDifferentVoiceChannel')] });
 
-                const skippedTrack = queue.nowPlaying();
-                const skipped = skip(queue, track);
-
-                if (typeof skipped == 'string') return interaction.reply({ embeds: [Player.getMessageEmbed(skipped)] });
+                const tracks = queue.tracks.length;
+                queue.clear();
 
                 interaction.reply({
                     embeds: [
-                        Player.getMessageEmbed(skipped ? 'skip' : 'error', skipped, skippedTrack.title, member.user.tag, member.user.id)
+                        Player.getMessageEmbed('clear', true, tracks.toString(), member.user.tag, member.user.id)
                     ]
                 });
             }),
         new MessageCommandBuilder()
-            .setName('skip')
-            .addOption(skipTo => skipTo
-                .setName('skip-to')
-                .setDescription('Skip to track number')
-                .setRequired(false)
-                .setValidator((val) => isNumber(val) && Number(val) >= 1)
-            )
+            .setName('clear')
             .setExecute(async command => {
                 const message = command.message;
                 const member = message.member;
                 const guild = message.guild;
-                const track = isNumber(command.command.args[0]) ? Number(command.command.args[0]) : undefined;
                 if (!guild || !member) return message.reply({ embeds: [Player.getMessageEmbed('notAMember')] });
 
                 const queue = Player.player.getQueue(guild);
                 if (!queue || queue.destroyed) return message.reply({ embeds: [Player.getMessageEmbed('noQueue')] });
                 if (member.voice.channelId !== guild.me?.voice.channelId) return message.reply({ embeds: [Player.getMessageEmbed('InDifferentVoiceChannel')] });
 
-                const skippedTrack = queue.nowPlaying();
-                const skipped = skip(queue, track);
-
-                if (typeof skipped == 'string') return message.reply({ embeds: [Player.getMessageEmbed(skipped)] });
+                const tracks = queue.tracks.length;
+                queue.clear();
 
                 message.reply({
                     embeds: [
-                        Player.getMessageEmbed(skipped ? 'skip' : 'error', skipped, skippedTrack.title, member.user.tag, member.user.id)
+                        Player.getMessageEmbed('clear', true, tracks.toString(), member.user.tag, member.user.id)
                     ]
                 });
             })
---
+++ src/EPlayerCommands/queue.ts
@@ -0,0 +1,97 @@
+import { ButtonType, OnDisableAction, Pagination, SendAs } from '@ghextercortes/djs-pagination';
+import { PaginationButton } from '@ghextercortes/djs-pagination/dist/util/Buttons';
+import { Queue } from 'discord-player';
+import { ColorResolvable, GuildMember, MessageButton, MessageEmbed, User } from 'discord.js';
+import { InteractionCommandBuilder, MessageCommandBuilder, recipleCommandBuilders } from 'reciple';
+import { EPlayer } from '../e-player';
+
+export default (Player: EPlayer): recipleCommandBuilders[] => {
+    const paginationButtons = new PaginationButton()
+            .addButton(ButtonType.FIRST_PAGE, new MessageButton().setCustomId('firstpage').setStyle('SECONDARY').setLabel('First'))
+            .addButton(ButtonType.PREVIOUS_PAGE, new MessageButton().setCustomId('previouspage').setStyle('PRIMARY').setLabel('Previous'))
+            .addButton(ButtonType.NEXT_PAGE, new MessageButton().setCustomId('nextpage').setStyle('SUCCESS').setLabel('Next'))
+            .addButton(ButtonType.LAST_PAGE, new MessageButton().setCustomId('lastpage').setStyle('SECONDARY').setLabel('Last'));
+
+    const getEmbeds = (queue: Queue, author: User): Pagination => {
+        let embedDescription = `Playing: **${queue.nowPlaying().title || 'none'}**\n`;
+        let embeds: MessageEmbed[] = [];
+        let page = 1;
+        let id = 0;
+        let i = 0;
+
+        for (const track of queue.tracks) {
+            i++;
+
+            embedDescription += `\n\`${id + 1}.\` **${track.title}** — <@${track?.requestedBy.id}>`;
+            id++;
+
+            if (i === 10 || i === queue.tracks.length) {
+                i = 0;
+                embeds.push(
+                    new MessageEmbed()
+                        .setAuthor({ name: `Queue`, iconURL: Player.client.user?.displayAvatarURL() })
+                        .setDescription(embedDescription)
+                        .setColor(Player.getMessage('embedColor') as ColorResolvable)
+                        .setFooter({ text: `Page ${page}`, iconURL: author.displayAvatarURL() })
+                );
+                embedDescription = `Playing: **${queue.nowPlaying().title || 'none'}**\n`;
+                page++;
+            }
+        }
+
+        const pagination = new Pagination()
+            .setAuthor(author)
+            .setOnDisableAction(OnDisableAction.DISABLE_BUTTONS)
+            .setTimer(20000)
+            .addPages(
+                embeds.length
+                ? embeds
+                : [
+                    new MessageEmbed()
+                        .setAuthor({ name: `Queue`, iconURL: Player.client.user?.displayAvatarURL() })
+                        .setDescription(`${embedDescription}\n*No tracks left*`)
+                        .setColor(Player.getMessage('embedColor') as ColorResolvable)
+                        .setFooter({ text: `Page 1`, iconURL: author.displayAvatarURL() })
+                ]
+            );
+
+        if (embeds.length) pagination.setButtons(paginationButtons);
+
+        return pagination;
+    }
+
+    return [
+        new InteractionCommandBuilder()
+            .setName('queue')
+            .setExecute(async command => {
+                const interaction = command.interaction;
+                const member = interaction.member as GuildMember;
+                const guild = interaction.guild;
+
+                if (!guild || !member) return interaction.reply({ embeds: [Player.getMessageEmbed('notAMember')] });
+
+                const queue = Player.player.getQueue(guild);
+                if (!queue || queue.destroyed) return interaction.reply({ embeds: [Player.getMessageEmbed('noQueue')] });
+
+                const pagination = getEmbeds(queue, member.user);
+
+                pagination.paginate(interaction, SendAs.REPLY_MESSAGE);
+            }),
+        new MessageCommandBuilder()
+            .setName('queue')
+            .setExecute(async command => {
+                const message = command.message;
+                const member = message.member;
+                const guild = message.guild;
+
+                if (!guild || !member) return message.reply({ embeds: [Player.getMessageEmbed('notAMember')] });
+
+                const queue = Player.player.getQueue(guild);
+                if (!queue || queue.destroyed) return message.reply({ embeds: [Player.getMessageEmbed('noQueue')] });
+
+                const pagination = getEmbeds(queue, member.user);
+
+                pagination.paginate(message, SendAs.REPLY_MESSAGE);
+            })
+    ];
+}
